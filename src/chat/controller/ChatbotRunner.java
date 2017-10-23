e6162f86323ce7fe6cc1d9654ce1ebbe9e13969c
boydstunryan <boydstunryan@gmail.com>
2017-10-23 09:57:48 -0600

    Initial commit

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,14 @@
+package chat.controller;
+
+import chat.view.PopupDisplay;
+
+public class ChatbotRunner 
+{
+	public static void main(String [] args)
+	{
+		PopupDisplay sample = new PopupDisplay();
+		sample.displayText("asd");
+		sample.collectResponse("asasdadada");
+	}
+
+}
